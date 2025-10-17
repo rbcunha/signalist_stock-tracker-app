26b3dabd6bca4f0687f48f35c14d8bcde279dbfe
rbcunha <barrosdacunha@gmail.com>
2025-10-17 16:38:06 -0300

    make the rabbit happy

--- components/SearchCommand.tsx
+++ components/SearchCommand.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react"
 import { CommandDialog, CommandEmpty, CommandInput, CommandList } from "@/components/ui/command"
 import {Button} from "@/components/ui/button";
-import {Loader2, Star, TrendingUp} from "lucide-react";
+import {Loader2, TrendingUp} from "lucide-react";
 import Link from "next/link";
 import {searchStocks} from "@/lib/actions/finnhub.actions";
 import {useDebounce} from "@/hooks/useDebounce";
